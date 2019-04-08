b3cdb28103cddb2b89a9402d0ce16c6a07c5b340
stevekutz <stkutz@gmail.com>
2019-04-07 22:03:15 -0500

    rebuilding App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App_done from './App_done';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App_done />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import App_done from './App_done';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
