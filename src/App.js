deaea163f038224d904acd589d58f5f507c4ad9e
stevekutz <stkutz@gmail.com>
2019-04-07 22:36:38 -0500

    building Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, {Component}from 'react';
 import './App.css';
 
-class App extends React.Component {
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+const User = () => {
+  return (
+    <h1> Welcome User</h1>
+
+  )
+
+};
+
+
+class App extends Component {
   render() {
     return (
-      <div className = "App">
+      <Router>
+
+
+        <div className = "App">
           <h1>Something</h1>
 
-      </div>
+        <Route
+          exact strict
+          path = "/"
+          render = { () => <h1>Welcome Home</h1>}
+        />
+
+        <Route
+          exact strict
+          path = "/about/"
+          render = { () => <h1>Welcome About</h1>}
+        />
+
+        <Route
+          exact strict
+          path = "/user"
+          render = { User }
+        />
+
+        </div>
+      </Router>
+
+
+
 
     )
 
